3bca0bf5fd0518b83285402e770353eba3f3c33c
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-14 23:58:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,15 @@ def nyc_pigeon_organizer(data)
   data.each do |property, hash|
     hash.each do |attribute, array|
       array.each do |name|
+        if !new_hash.include(name)
+          new_hash[name] = {}
+        end
         
+        if !new_hash[name].include(attribute)
+          new_hash[name][attribute] = []
+        end
+        
+        if !new_hash[name]
       end
     end
   end
